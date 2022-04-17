a9c3589ba6947aee0d1588aede85d67294b852c3
Nikita Kotsehub <47567310+nikita-kotsehub@users.noreply.github.com>
2022-04-17 22:43:01 +0200

    added multichoice and natural language representation

--- expert/expert.py
+++ expert/expert.py
@@ -23,7 +23,81 @@ def write_py(X):
 
 def read_py(A,V,Y):
     if isinstance(Y, Variable):
-        response = input(str(A) + " is " + str(V) + "? ")
+        if str(A) == "body_temperature":
+            print("Please measure your body temperature. Choose the option that most closely describes it:")
+            print("a. High \nb. Regular")
+            answer = input("Your body temperature is: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+            
+        elif str(A) == "chest":
+            print("Is your chest in pain?")
+            print("a. yes, it is \nb. no, it's not")
+            answer = input("Choose a or b to describe your chest condition: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+
+        elif str(A) == "breathing":
+            print(f"Is {str(A)} {str(V)} for you?")
+            print("a. yes, it is \nb. no, it's not")
+            answer = input("Choose your breathing condition: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+
+        elif str(A) == "cough":
+            print(f"If coughing, how would you describe your {str(A)}?")
+            print("a. Strong \nb. Medium \nc. Not too bad")
+            answer = input("Choose your coughing condition: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+
+        elif str(A) == "speech":
+            print(f"Do you experience difficulties with {str(A)}?")
+            print("a. Yes, I do :( \nb. No, all good")
+            answer = input(f"Choose your {str(A)} condition: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+
+        elif str(A) == "taste":
+            print(f"Can you {str(A)}?")
+            print("a. Yes, I can ^-^ \nb. No, not at all :(")
+            answer = input(f"Choose your {str(A)} condition: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+
+        elif str(A) == "smell":
+            print(f"Can you {str(A)}?")
+            print("a. Yes, I can ^-^ \nb. No, not at all :(")
+            answer = input(f"Choose your {str(A)} condition: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+
+        elif str(A) == "throat":
+            print(f"Is your {str(A)} {str(V)}?")
+            print("a. Yes, it is \nb. Nope")
+            answer = input(f"Choose your {str(A)} condition: ")
+            if answer == "a":
+                response="yes"
+            else:
+                response="no"
+
+        else:
+            response = input(str(A) + " is " + str(V) + "? ")
+
         Y.unify(Atom(response))
         return True
     else:
@@ -42,5 +116,5 @@ disease = [s for s in prolog.query("disease(X).", maxresult=1)]
 contact = [s for s in prolog.query("contact(X).")]
 test = [s for s in prolog.query("test(X).")]
 print("Your disease is " + (disease[0]['X'] + "." if disease else "unknown."))
-print("You need to urgently contact " + str(set(c['X'] for c in contact)) if contact else "You don't need to contact anyone")
+print("You need to urgently contact " + str(' and '.join(set(c['X'] for c in contact))) if contact else "You don't need to contact anyone")
 print(("You also need to do an " + test[0]['X'] + " test" if test else "No testing is needed"))
